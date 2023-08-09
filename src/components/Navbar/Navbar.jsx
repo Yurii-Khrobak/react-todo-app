f2071a492e8ede11794dba8904a53b1e3ba3423b
Yurii-Khrobak <khrobakyuriuwork@gmail.com>
2023-08-09 10:00:49 +0300

    Deployment

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,6 +6,7 @@ const Navbar = ({setVisible}) => {
   return (
     <div className={cl.navbar}>
       <a className={cl.logo}>Todos<div className={cl.logo__react}>RC</div></a>
+      <a className={cl.link} href='https://github.com/Yurii-Khrobak/react-todo-app'>GitHub</a>
       <MyButton onClick={() => setVisible(true)}>Add todo</MyButton>
     </div>
   )
